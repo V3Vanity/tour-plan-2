0f8092079e2d0fd169c28c60608a62efcdf0bce2
V3Vanity <jun30010kmtn.ruu@gmail.com>
2024-04-25 11:54:09 +0300

    create mobile menu

--- js/main.js
+++ js/main.js
@@ -28,3 +28,11 @@ const reviewsSlider = new Swiper(".reviews-slider", {
     pageUpDown: true,
   },
 });
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  console.log("Клик по кнопке меню");
+  document
+    .querySelector(".navbar-bottom")
+    .classList.toggle("navbar-bottom--visible");
+});
